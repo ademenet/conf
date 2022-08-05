b57f5cbdc1635ac3e6c32ad472f9f8e72bd6c1e3
Alain Demenet <9986466+ademenet@users.noreply.github.com>
2022-08-05 15:19:29 +0200

    ADD Save folders script

---
+++ save-folders.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Save Work folders
+# a for archive, h for human readable, P for progress, v for verbose
+rsync \
+  -ahPv \
+  --stats \
+  --exclude "node_modules" \
+  --exclude "virt" \
+  --exclude "venv" \
+  /Users/ademenet/Work \
+  /Volumes/ALAIN_HDD/ademenet/ \
+  2> /tmp/rsyncErrors.txt
+
+# Save notes folders
+rsync \
+  -ahPv \
+  --stats \
+  --exclude "node_modules" \
+  --exclude "virt" \
+  --exclude "venv" \
+  /Users/ademenet/jedha-notes \
+  /Volumes/ALAIN_HDD/ademenet/ \
+  2> /tmp/rsyncErrors.txt
+
+rsync \
+  -ahPv \
+  --stats \
+  --exclude "node_modules" \
+  --exclude "virt" \
+  --exclude "venv" \
+  /Users/ademenet/personal-notes \
+  /Volumes/ALAIN_HDD/ademenet/ \
+  2> /tmp/rsyncErrors.txt
+
